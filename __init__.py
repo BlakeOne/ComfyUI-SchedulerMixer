d74262757764065a74a804b336bce787b288939b
BlakeOne <30273164+BlakeOne@users.noreply.github.com>
2024-03-31 17:59:18 -0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .node import SchedulerMixer
+
+NODE_CLASS_MAPPINGS = {"SchedulerMixer": SchedulerMixer}
+NODE_DISPLAY_NAME_MAPPINGS = {"SchedulerMixer" : "SchedulerMixer"}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
